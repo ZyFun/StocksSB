e7b764ae6d590fe607bda0530e289839b2adf5e1
Dmitriy Danilin <danilindn@icloud.com>
2021-09-24 16:15:07 +0300

    chore: add data manager and prepare table view controller

---
+++ Stock/Services/DataManager.swift
@@ -0,0 +1,17 @@
+//
+//  DataManager.swift
+//  Stock
+//
+//  Created by Дмитрий Данилин on 24.09.2021.
+//
+
+class DataManager {
+    
+    let company = [
+        "Apple": "AAPL",
+        "Microsoft": "MSFT",
+        "Google": "GOOG",
+        "Amazon": "AMZN",
+        "Facebook": "FB",
+    ]
+}
