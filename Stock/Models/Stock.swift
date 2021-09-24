4885de1e1bcc90d8fd50bb91468bdcf9c77bda5f
Dmitriy Danilin <danilindn@icloud.com>
2021-09-24 16:07:09 +0300

    chore: add model

---
+++ Stock/Models/Stock.swift
@@ -0,0 +1,13 @@
+//
+//  Stock.swift
+//  Stock
+//
+//  Created by Дмитрий Данилин on 24.09.2021.
+//
+
+struct Stock {
+    let companyName: String
+    let symbol: String
+    let latestPrice: Double
+    let change: Double
+}
