98be8af6fcbe26ef174e7e76767e2244933e10d8
Dmitriy Danilin <danilindn@icloud.com>
2021-09-24 18:44:04 +0300

    chore: prepare table view for testing display data

--- Stock/ViewControllers/StockListViewController.swift
+++ Stock/ViewControllers/StockListViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class StockListViewController: UITableViewController {
+    
+    var stocks: [Stock] = Stock.getStock()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,25 +23,19 @@ class StockListViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        stocks.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StockCell", for: indexPath) as! StockCell
+
+        let stock = stocks[indexPath.row]
+        cell.configure(with: stock)
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
