afed9b60f84338f4c436ea08d07f23856347b807
Dmitriy Danilin <danilindn@icloud.com>
2021-09-24 17:18:37 +0300

    chore: add custom cell

---
+++ Stock/ViewControllers/StockCell.swift
@@ -0,0 +1,33 @@
+//
+//  StockCell.swift
+//  Stock
+//
+//  Created by Дмитрий Данилин on 24.09.2021.
+//
+
+import UIKit
+
+class StockCell: UITableViewCell {
+    
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var changePriceLabel: UILabel!
+    
+    func configure(with stock: Stock) {
+        companyNameLabel.text = stock.companyName
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
