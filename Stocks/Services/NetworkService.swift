ab6cd4b44990719cd0439b84adfa31c50f90cb3e
Dmitriy Danilin <danilindn@icloud.com>
2021-09-26 17:27:16 +0300

    chore: add network service

---
+++ Stocks/Services/NetworkService.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkService.swift
+//  Stocks
+//
+//  Created by Дмитрий Данилин on 25.09.2021.
+//
+
+import Foundation
+
+class NetworkService {
+    static let shared = NetworkService()
+    
+    func request(urlString: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                print(error)
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else { return }
+            completion(.success(data))
+        }.resume()
+        
+    }
+    
+    private init(){}
+}
