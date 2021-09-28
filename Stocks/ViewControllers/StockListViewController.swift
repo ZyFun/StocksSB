90e0f66ae90e33a72cf1b618a492f9d40e124716
Dmitriy Danilin <danilindn@icloud.com>
2021-09-29 00:05:28 +0300

    chore: add todo

--- Stocks/ViewControllers/StockListViewController.swift
+++ Stocks/ViewControllers/StockListViewController.swift
@@ -56,6 +56,7 @@ class StockListViewController: UITableViewController {
 extension StockListViewController {
     // TODO: Вывести алерт при отсутствии сети
     private func getStocks() {
+        // TODO: Убрать ключь в отдельный безопасный файл и добавить его в гит игнор (добавить в проект гитигнор) В дальнейшем делать так со всеми ключами. Данный токен уничтожить и сгенерировать новый. Почитать подробнее о том, как хранить токены.
         let token = "pk_92287e65be054541b0a167b0ac4fa0aa"
         
         activityIndicator.startAnimating()
