808c4f297b9e4c70e33592b4432886cd21bfc21b
Dmitriy Danilin <danilindn@icloud.com>
2021-10-03 16:34:02 +0300

    chore: add documentation

--- Stocks/Services/Token.swift
+++ Stocks/Services/Token.swift
@@ -6,7 +6,7 @@
 //
 
 enum Token: String {
-    // Replace the token with your own
-    // https://iexcloud.io
+    /// Replace the token with your own
+    /// https://iexcloud.io
     case publicKey = "pk_92287e65be054541b0a167b0ac4fa0aa"
 }
