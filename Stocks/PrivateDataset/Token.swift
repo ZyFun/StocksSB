a31641b6f70bd7baf1645e48901e660f879add36
Dmitriy Danilin <danilindn@icloud.com>
2021-10-03 16:12:35 +0300

    chore: add token file

---
+++ Stocks/PrivateDataset/Token.swift
@@ -0,0 +1,12 @@
+//
+//  Token.swift
+//  Stocks
+//
+//  Created by Дмитрий Данилин on 03.10.2021.
+//
+
+enum Token: String {
+    // Replace the token with your own
+    // https://iexcloud.io
+    case publicKey = "pk_92287e65be054541b0a167b0ac4fa0aa"
+}
