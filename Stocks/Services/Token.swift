233630b294629660081078730177079f8e53358b
Dmitriy Danilin <danilindn@icloud.com>
2021-10-03 16:36:02 +0300

    refactor: edit documentation

--- Stocks/Services/Token.swift
+++ Stocks/Services/Token.swift
@@ -7,6 +7,6 @@
 
 enum Token: String {
     /// Replace the token with your own
-    /// https://iexcloud.io
+    /// - you can get it here: https://iexcloud.io
     case publicKey = "pk_92287e65be054541b0a167b0ac4fa0aa"
 }
