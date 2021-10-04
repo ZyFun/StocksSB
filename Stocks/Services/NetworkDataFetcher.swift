21e6edfc6492b9b747b1e59f6af928aa01a0b2d6
Dmitriy Danilin <danilindn@icloud.com>
2021-10-04 13:28:00 +0300

    chore: Add method for loading an image with receiving a response from the server

--- Stocks/Services/NetworkDataFetcher.swift
+++ Stocks/Services/NetworkDataFetcher.swift
@@ -45,6 +45,23 @@ class NetworkDataFetcher {
         }
     }
     
+    func fetchImageCache(from url: URL, completion: @escaping (Data, URLResponse) -> Void) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let response = response else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            guard url == response.url else { return }
+            
+            DispatchQueue.main.async {
+                completion(data, response)
+            }
+
+        }.resume()
+    }
+    
     private init(){}
     
 }
